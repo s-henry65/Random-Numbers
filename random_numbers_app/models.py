b1641d9878a93d2cd53da31ec44de28fd2397e1b
Scott <scott.henry.moore@gmail.com>
2023-02-20 19:50:43 -0800

    changing git ignore

--- random_numbers_app/models.py
+++ random_numbers_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class oregon_results(models.Model):
+    draw_date = models.CharField(max_length=10)
+    numbers = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.draw_date + ', ' + self.numbers
+    
+    class Meta:
+        ordering = ('draw_date',)
